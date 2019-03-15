be0b66c154f8e337d975054cf7fc1134d5b0e0a3
prescottbreeden <prescott.breeden@gmail.com>
2019-03-15 16:06:12 -0700

    Prescott's Code Review
    Discussed javascript syntax guide rules stuff, tweaked api route
    structure in routes, renamed controller functions, expanded models to
    individual lines, and many beers were had by all.

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-// ---------- Set - Up ----------
 const express = require('express');
 const bodyParser = require('body-parser');
+const port = 8000;
 const app = express();
 
 app.use(bodyParser.json());
+app.listen(port, () => console.log(`Listening on port ${port}`));
 require('./backend/routes')(app);
-
-// ---------- Server ----------
-app.listen(8000, function(){
-    console.log('localhost:8000')
-})
