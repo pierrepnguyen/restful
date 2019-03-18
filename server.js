8833c80a796f7df7b7e3471c74b17d82fb02bafc
Pierre Nguyen <pierrenguyen@Pierres-MacBook-Pro.local>
2019-03-18 15:06:28 -0700

    first commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+app.use(express.static( __dirname + '/NinjaGold/dist/NinjaGold' ));
 app.use(bodyParser.json());
 require('./backend/routes')(app);
 
